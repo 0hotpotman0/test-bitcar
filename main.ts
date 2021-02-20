2e04c6751ceea9bbbbe28a3db958790045e1e636
fealy <47217650+0hotpotman0@users.noreply.github.com>
2021-02-20 18:51:35 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+BitCar.OnKey(BitPlayerKey.key_B, BitPlayerKeyEvent.click, function () {
+    basic.showIcon(IconNames.Yes)
+})
+BitCar.OnKey(BitPlayerKey.key_A, BitPlayerKeyEvent.click, function () {
+    basic.showIcon(IconNames.Heart)
+})
+basic.forever(function () {
+	
+})
